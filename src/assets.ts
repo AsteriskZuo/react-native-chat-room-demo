d58b7b6706b8f728db0d03710647646dd425e370
asteriskzuo <zuoyuhsywn@hotmail.com>
2023-12-01 00:50:29 +0800

    chore: update

--- src/assets.ts
+++ src/assets.ts
@@ -5,4 +5,4 @@ export const a_easemob_logo_text = require('./assets/easemob_logo_text.png');
 export const a_lightImage = require('./assets/390w_light_2x.png');
 export const a_darkImage = require('./assets/390w_dark_2x.png');
 
-export const a_video = require('./assets/video_easemob.mp4');
+export const a_video = require('./assets/video_easemob.mov');
